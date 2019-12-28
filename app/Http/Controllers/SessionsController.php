b3b6b9a6162a091706193a38294f763fe9d33e1d
Insomnia Zhang <zhangshimiande@gmail.com>
2019-12-28 22:18:07 +0800

    实现自动登录功能

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -9,6 +9,9 @@ class SessionsController extends Controller
 {
     public function create()
     {
+        if (Auth::user()) {
+            return redirect()->route('users.show', [Auth::user()->id]);
+        }
         return view('sessions.create');
     }
 
