0747368c1f9f6ce78218fad6873c3918f28d07f3
Insomnia Zhang <zhangshimiande@gmail.com>
2019-12-28 21:42:05 +0800

    创建会话

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -6,7 +6,7 @@
       <a class="nav-link" href="{{ route('help') }}">帮助</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">登录</a>
+        <a class="nav-link" href="{{ route('login') }}">登录</a>
       </li>
     </ul>
   </div>
