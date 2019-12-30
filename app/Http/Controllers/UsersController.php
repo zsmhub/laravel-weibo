9d0be3aaf0d08e17928242ec3a4e291c5a2f0d89
Insomnia Zhang <zhangshimiande@gmail.com>
2019-12-30 15:58:13 +0800

    更新用户

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,14 +8,14 @@ use Auth;
 
 class UsersController extends Controller
 {
-    public function create()
-    {
-        return view('users.create');
-    }
-
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    public function create()
+    {
+        return view('users.create');
     }
 
     public function store(Request $request)
@@ -35,5 +35,39 @@ class UsersController extends Controller
         Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user->id]);
+    }
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show', $user->id);
+    }
+
+    public function destroy()
+    {
+
+    }
+
+    public function index()
+    {
+
     }
 }
