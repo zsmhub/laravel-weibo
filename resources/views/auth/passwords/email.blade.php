accb41446b4c9b6589fb88b40f8fd6e72d3fe3a4
Insomnia Zhang <zhangshimiande@gmail.com>
2019-12-31 16:07:25 +0800

    移除邮件发送的 from 选项

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -7,12 +7,6 @@
     <div class="card-header"><h5>重置密码</h5></div>
 
     <div class="card-body">
-      @if (session('status'))
-      <div class="alert alert-success">
-        {{ session('status') }}
-      </div>
-      @endif
-
       <form class="" method="POST" action="{{ route('password.email') }}">
         {{ csrf_field() }}
 
