c2ab5c94fd83773c86cb58819442d4b82cf4a9a4
Insomnia Zhang <zhangshimiande@gmail.com>
2020-01-02 22:38:16 +0800

    粉丝数据表逻辑

--- app/Models/User.php
+++ app/Models/User.php
@@ -63,6 +63,45 @@ class User extends Authenticatable
     public function feed()
     {
         return $this->statuses()
-                    ->orderBy('created_at', 'desc');
+            ->orderBy('created_at', 'desc');
+    }
+
+    /**
+     * 获取粉丝关系列表
+     * @return object
+     */
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    /**
+     * 获取用户关注人列表
+     * @return object
+     */
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contain($user_id);
     }
 }
---
+++ database/migrations/2020_01_02_213107_create_folowers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class CreateFolowersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('followers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id')->index()->comment('粉丝id');
+            $table->integer('follower_id')->index()->comment('关注人id');
+            $table->timestamps();
+        });
+
+        DB::statement("ALTER TABLE `followers` comment '粉丝表'");  // 表注释
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('followers');
+    }
+}
